4d75308505ecd8f9706b81ce3c33e4f8db6a013d
alejandrosibaja14 <sibaja.alejandro@icloud.com>
2026-06-26 20:12:23 -0600

    Creación de API y función obtenerVehiculos. Avance #3

--- funciones.py
+++ funciones.py
@@ -3,3 +3,33 @@
 #Ultima modificación:
 #Versión de python: 3.14
 from clases import *
+import requests
+vehiculosAPI="https://my.api.mockaroo.com/vehiculos.json?key=7427d5e0"
+
+def obtenerVehiculos():
+    """
+    Funcionalidad: Obiene los vehículos desde Mockaroo y construye el diccionario solicitado.
+    Entradas: N/A
+    Salidas: Diccionario de vehículos
+    """
+    try:
+        respuesta=requests.get(vehiculosAPI)
+        if respuesta.status_code!=200:
+            return False,{}
+        datos=respuesta.json()
+        diccionarioVehiculos={}
+        for vehiculo in datos:
+            placa=vehiculo["placa"]
+            diccionarioVehiculos[placa]={
+                "marca":vehiculo["marca"],
+                "color":vehiculo["color"],
+                "tipo":vehiculo["tipo"],
+                "ubicacion":"",
+                "fechaHoraEntrada":"",
+                "fechaHoraSalida":"",
+                "monto":0,
+                "tipoPago":0
+            }
+        return True,diccionarioVehiculos
+    except Exception:
+        return False,{}
